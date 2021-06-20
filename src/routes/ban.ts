4819a7cbb749e6b1e644255f328fdb4bdb108a53
Damien Vesper <ldamienvesper@gmail.com>
2021-06-20 14:52:37 -0500

    ban router -> router

--- src/routes/ban.ts
+++ src/routes/ban.ts
@@ -2,20 +2,20 @@ import * as Express from 'express';
 
 import Ban from '../models/ban.model';
 
-const banRouter: Express.Router = Express.Router();
+const router: Express.Router = Express.Router();
 
-banRouter.get(`*`, async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+router.get(`*`, async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
     const isBanned = await Ban.findOne({ IP: req.ip });
 
     if (isBanned) return;
     next();
 });
 
-banRouter.all(`*`, async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+router.all(`*`, async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
     const isBanned = await Ban.findOne({ IP: req.ip });
 
     if (isBanned) return res.send(`403 Forbidden`).status(403);
     next();
 });
 
-export default banRouter;
+export default router;
